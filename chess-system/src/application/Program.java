bf8b9020f234987091fb873ed909893139a97c4a
gefersonholdorf <gefersonholdorf88@gmail.com>
2022-03-03 20:49:28 -0300

    First class: Position

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Position p1 = new Position(5, 2);
+		System.out.println(p1);
 
 	}
 
